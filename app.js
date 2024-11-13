cc80cc1eaabcd95557c4cbb56db9140c04d7cf54
Elvin251 <emmmdov485@gmail.com>
2024-11-13 16:41:27 +0400

    d

---
+++ app.js
@@ -0,0 +1,8 @@
+let number = Number(prompt("Bir ədəd daxil edin:"));
+if (number < 10) {
+    console.log("10-dan kiçikdir");
+} else if (number === 10) {
+    console.log("10-a bərabərdir");
+} else {
+    console.log("10-dan böyükdür");
+}
